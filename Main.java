e28e30e0451315ca66e0aec1e9fcff2783bc607a
Vi Minh Hiển <164661434+minhshiens@users.noreply.github.com>
2025-07-24 00:08:48 +0700

    mói mói mói

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // This is the entry point of the Java application
+        System.out.println("Hello, World!");
+    }
+    
+}
