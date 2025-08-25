5aa99462661216c5ae44be944a085a1221e8ec9e
HgNam123456 <166427581+HgNam123456@users.noreply.github.com>
2025-08-25 21:19:41 +0700

    Update DatabaseConnector.java
    fix database connector

--- backend/src/main/java/backend/driver/DatabaseConnector.java
+++ backend/src/main/java/backend/driver/DatabaseConnector.java
@@ -12,7 +12,7 @@ import io.javalin.Javalin;
 public class DatabaseConnector {
     private static final String JDBC_URL = "jdbc:mysql://127.0.0.1:3306/lib?useSSL=false&serverTimezone=UTC";
     private static final String JDBC_USER = "root";
-    private static final String JDBC_PASSWORD = "1109"; // Đảm bảo mật khẩu này đúng
+    private static final String JDBC_PASSWORD = "ngocnam2005"; // Đảm bảo mật khẩu này đúng
 
     static {
         try {
